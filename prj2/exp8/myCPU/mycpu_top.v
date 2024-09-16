454b11d784c3926f14741763463f8a252da5f5d7
Wang Zeli <ZeriWang@users.noreply.github.com>
2024-09-16 23:19:00 +0800

    Add: Blocking & Forwarding

--- prj2/exp8/myCPU/mycpu_top.v
+++ prj2/exp8/myCPU/mycpu_top.v
@@ -131,20 +131,11 @@ reg  [ 4:0] pipe_valid;  // IF ID EX MEM WB
 wire [ 4:0] pipe_allowin;
 wire [ 4:0] pipe_ready_go;
 wire [ 3:0] pipe_tonext_valid;
-wire [ 4:0] pipe_stay;
-wire [ 4:0] pipe_refresh;
-wire [ 4:0] pipe_leave;
-wire [ 4:0] pipe_enter;
 
-assign pipe_allowin[ 3:0] = ~pipe_valid[ 3:0]
-                                       | pipe_ready_go[ 3 :0] & pipe_allowin[ 4 :1];
+assign pipe_allowin[ 3:0] = ~pipe_valid[ 3:0] | pipe_ready_go[ 3:0] & pipe_allowin[ 4:1];
 assign pipe_allowin[4] = ~pipe_valid[4] | pipe_ready_go[4];
                                        
-assign pipe_tonext_valid[ 3 :0] = pipe_allowin[ 4 :1] & pipe_ready_go[ 3 :0];
-assign pipe_stay    = pipe_valid & ~{1'b1, pipe_tonext_valid};
-assign pipe_refresh = pipe_valid & {pipe_tonext_valid, 1'b1}; // NOTE: 1'b1 is for PC refresh
-assign pipe_leave   = pipe_valid & {1'b1, pipe_tonext_valid};
-assign pipe_enter   = {pipe_valid[3:0], 1'b1} & {pipe_tonext_valid, pipe_allowin[0]}; // NOTE: 1'b1 is for PC refresh
+assign pipe_tonext_valid[ 3:0] = pipe_allowin[ 4:1] & pipe_ready_go[ 3:0];
 
 // valid signal control in pipeline
 always @(posedge clk) begin
@@ -152,7 +143,29 @@ always @(posedge clk) begin
         pipe_valid <= 5'b00000;
     end
     else begin
-        pipe_valid <= {pipe_tonext_valid, pipe_allowin[0]} | pipe_stay;
+        if (pipe_allowin[0]) begin
+                pipe_valid[0] <= 1'b1;
+        end
+        if (br_taken) begin
+            pipe_valid[1] <= 1'b0;
+            if (pipe_tonext_valid[2]) begin
+                pipe_valid[2] <= 1'b0;
+            end
+        end
+        else  begin
+            if (pipe_allowin[1]) begin
+                pipe_valid[1] <= pipe_ready_go[0];
+            end
+            if (pipe_allowin[2]) begin
+                pipe_valid[2] <= pipe_ready_go[1];
+            end
+        end
+        if (pipe_allowin[3]) begin
+            pipe_valid[3] <= pipe_ready_go[2];
+        end
+        if (pipe_allowin[4]) begin
+            pipe_valid[4] <= pipe_ready_go[3];
+        end
     end
 end
 
@@ -167,8 +180,39 @@ always @(posedge clk) begin
     if (reset) begin
         pc <= 32'h1bfffffc;     //trick: to make nextpc be 0x1c000000 during reset 
     end
-    else begin
+    else if (br_taken) begin
         pc <= nextpc;
+    end
+    else if (pipe_allowin[0])begin
+        pc <= nextpc;
+    end
+end
+
+assign inst = inst_sram_rdata;  // instruction memory read data
+
+// store the first instruction in the pipeline when IF stays more than 1 cycle
+
+reg first_IF; // signal for the first instruction in the pipeline
+reg [31:0] inst_IF_reg; // register for store the instruction
+
+always @(posedge clk) begin
+    if (reset) begin
+        first_IF <= 1'b1;
+    end
+    else if (br_taken) begin
+        first_IF <= 1'b1;
+    end
+    else if (pipe_tonext_valid[0]) begin
+        first_IF <= 1'b1;
+    end
+    else begin
+        first_IF <= !pipe_valid[0];
+    end
+end
+
+always @(posedge clk) begin
+    if (first_IF) begin
+        inst_IF_reg <= inst;
     end
 end
 
@@ -181,7 +225,6 @@ assign inst_sram_en    = 1'b1;  // instruction memory enable
 assign inst_sram_we    = 4'b0;  // instruction memory strb
 assign inst_sram_addr  = nextpc;  // instruction memory address
 assign inst_sram_wdata = 32'b0;  // instruction memory write data
-assign inst            = inst_sram_rdata;  // instruction memory read data
 
 assign pipe_ready_go[0] = pipe_valid[0];
 
@@ -195,8 +238,15 @@ reg  [31:0] inst_ID;
 reg  [31:0] pc_ID;
 
 always @(posedge clk) begin
-    inst_ID <= inst;
-    pc_ID   <= pc;
+    if (pipe_tonext_valid[0]) begin
+        if (first_IF) begin
+            inst_ID <= inst;
+        end else begin
+            inst_ID <= inst_IF_reg;
+        end
+
+        pc_ID   <= pc;
+    end
 end
 
 
@@ -245,8 +295,7 @@ assign inst_beq    = op_31_26_d[6'h16];
 assign inst_bne    = op_31_26_d[6'h17];
 assign inst_lu12i_w= op_31_26_d[6'h05] & ~inst_ID[25];
 
-assign alu_op[ 0] = inst_add_w | inst_addi_w | inst_ld_w | inst_st_w
-                    | inst_jirl | inst_bl;
+assign alu_op[ 0] = inst_add_w | inst_addi_w | inst_ld_w | inst_st_w | inst_jirl | inst_bl;
 assign alu_op[ 1] = inst_sub_w;
 assign alu_op[ 2] = inst_slt;
 assign alu_op[ 3] = inst_sltu;
@@ -308,11 +357,114 @@ regfile u_regfile(
     .wdata  (rf_wdata )
     );
 
-assign rj_value  = rf_rdata1;
-assign rkd_value = rf_rdata2;
+// signals for blocking control
+wire rf_using1;
+wire rf_using2;
 
- assign pipe_ready_go[1] = pipe_valid[1];
+assign rf_using1 = inst_beq 
+                || inst_bne 
+                || inst_jirl // branch instructions without b/bl
+                || inst_ld_w 
+                || inst_st_w // load/store instructions
+                || inst_add_w
+                || inst_sub_w
+                || inst_slt
+                || inst_sltu
+                || inst_nor
+                || inst_and
+                || inst_or
+                || inst_xor // ALU without imm instructions
+                || inst_slli_w
+                || inst_srli_w
+                || inst_srai_w
+                || inst_addi_w // imm instructions
+;
 
+assign rf_using2 = inst_beq 
+                || inst_bne 
+                || inst_jirl // branch instructions without b/bl
+                // || inst_ld_w 
+                // || inst_st_w // load/store instructions
+                || inst_add_w
+                || inst_sub_w
+                || inst_slt
+                || inst_sltu
+                || inst_nor
+                || inst_and
+                || inst_or
+                || inst_xor // ALU without imm instructions
+                // || inst_slli_w
+                // || inst_srli_w
+                // || inst_srai_w
+                // || inst_addi_w // imm instructions
+;
+
+assign rj_value  = df_alu_r1_EX  ? alu_result     :
+                   df_alu_r1_MEM ? alu_result_MEM :
+                   df_alu_r1_WB  ? alu_result_WB  :
+                   df_ld_r1_EX   ? mem_result     :
+                   rf_rdata1
+;
+
+assign rkd_value = df_alu_r2_EX  ? alu_result     :
+                   df_alu_r2_MEM ? alu_result_MEM :
+                   df_alu_r2_WB  ? alu_result_WB  :
+                   df_ld_r2_EX   ? mem_result     :
+                   rf_rdata2
+;
+
+
+// Pipeline blocking control
+// signals for data forwarding
+
+wire rd_eq_r1_EX;
+wire rd_eq_r1_MEM;
+wire rd_eq_r1_WB;
+wire rd_eq_r2_EX;
+wire rd_eq_r2_MEM;
+wire rd_eq_r2_WB;
+
+assign rd_eq_r1_EX  = (rf_raddr1 == dest_EX)  && (rf_raddr1 != 5'h0) && pipe_valid[2];
+assign rd_eq_r1_MEM = (rf_raddr1 == dest_MEM) && (rf_raddr1 != 5'h0) && pipe_valid[3];
+assign rd_eq_r1_WB  = (rf_raddr1 == dest_WB)  && (rf_raddr1 != 5'h0) && pipe_valid[4];
+assign rd_eq_r2_EX  = (rf_raddr2 == dest_EX)  && (rf_raddr2 != 5'h0) && pipe_valid[2];
+assign rd_eq_r2_MEM = (rf_raddr2 == dest_MEM) && (rf_raddr2 != 5'h0) && pipe_valid[3];
+assign rd_eq_r2_WB  = (rf_raddr2 == dest_WB)  && (rf_raddr2 != 5'h0) && pipe_valid[4];
+
+wire df_alu_r1_EX;
+wire df_alu_r1_MEM;
+wire df_alu_r1_WB;
+wire df_alu_r2_EX;
+wire df_alu_r2_MEM;
+wire df_alu_r2_WB;
+wire df_ld_r1_EX;
+wire df_ld_r1_MEM;
+wire df_ld_r1_WB;
+wire df_ld_r2_EX;
+wire df_ld_r2_MEM;
+wire df_ld_r2_WB;
+
+assign df_alu_r1_EX  = rd_eq_r1_EX  && gr_we_EX  && !inst_ld_w_EX;
+assign df_alu_r1_MEM = rd_eq_r1_MEM && gr_we_MEM && !inst_ld_w_MEM;
+assign df_alu_r1_WB  = rd_eq_r1_WB  && gr_we_WB  && !inst_ld_w_WB;
+assign df_alu_r2_EX  = rd_eq_r2_EX  && gr_we_EX  && !inst_ld_w_EX;
+assign df_alu_r2_MEM = rd_eq_r2_MEM && gr_we_MEM && !inst_ld_w_MEM;
+assign df_alu_r2_WB  = rd_eq_r2_WB  && gr_we_WB  && !inst_ld_w_WB;
+assign df_ld_r1_EX   = rd_eq_r1_EX  && inst_ld_w_EX;
+assign df_ld_r1_MEM  = rd_eq_r1_MEM && inst_ld_w_MEM;
+assign df_ld_r1_WB   = rd_eq_r1_WB  && inst_ld_w_WB;
+assign df_ld_r2_EX   = rd_eq_r2_EX  && inst_ld_w_EX;
+assign df_ld_r2_MEM  = rd_eq_r2_MEM && inst_ld_w_MEM;
+assign df_ld_r2_WB   = rd_eq_r2_WB  && inst_ld_w_WB;
+
+wire ID_stay;
+
+assign ID_stay   = df_ld_r1_EX   && rf_using1
+                || df_alu_r1_MEM && rf_using1
+                || df_ld_r2_EX   && rf_using2
+                || df_alu_r2_MEM && rf_using2
+
+assign pipe_ready_go[1] = pipe_valid[1] && !ID_stay;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ID stage to EX stage
@@ -334,8 +486,8 @@ reg  [11:0] alu_op_EX;
 // reg  inst_srli_w_EX;
 // reg  inst_srai_w_EX;
 // reg  inst_addi_w_EX;
-// reg  inst_ld_w_EX;
-// reg  inst_st_w_EX;
+reg  inst_ld_w_EX;
+reg  inst_st_w_EX;
 reg  inst_jirl_EX;
 reg  inst_b_EX;
 reg  inst_bl_EX;
@@ -372,8 +524,8 @@ always @(posedge clk) begin
 //    inst_srli_w_EX  <= inst_srli_w;
 //    inst_srai_w_EX  <= inst_srai_w;
 //    inst_addi_w_EX  <= inst_addi_w;
-//    inst_ld_w_EX    <= inst_ld_w;
-//    inst_st_w_EX    <= inst_st_w;
+    inst_ld_w_EX    <= inst_ld_w;
+    inst_st_w_EX    <= inst_st_w;
     inst_jirl_EX    <= inst_jirl;
     inst_b_EX       <= inst_b;
     inst_bl_EX      <= inst_bl;
@@ -398,6 +550,18 @@ end
 // EX stage
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+reg first_EX // signal for the first instruction in the pipeline
+always @(posedge clk) begin
+    if(reset) begin
+        first_EX <= 1'b1;
+    end
+    else if(pipe_tonext_valid[1]) begin
+        first_EX <= 1'b1;
+    end
+    else begin
+        first_EX <= 1'b0;
+    end
+end
 
 assign rj_eq_rd = (rj_value_EX == rkd_value_EX);
 assign br_taken = (   inst_beq_EX  &&  rj_eq_rd
@@ -405,7 +569,7 @@ assign br_taken = (   inst_beq_EX  &&  rj_eq_rd
                    || inst_jirl_EX
                    || inst_bl_EX
                    || inst_b_EX
-                  ) && pipe_valid[2];
+                  ) && pipe_valid[2] && first_EX;
 assign br_target = (inst_beq_EX || inst_bne_EX || inst_bl_EX || inst_b_EX) ? (pc_EX + br_offs_EX) :
                                                    /*inst_jirl*/ (rj_value_EX + jirl_offs_EX);
 
@@ -435,6 +599,9 @@ assign pipe_ready_go[2] = pipe_valid[2];
 reg  [31:0] pc_MEM;
 reg  [31:0] alu_result_MEM;
 
+reg         inst_ld_w_MEM;
+reg         inst_st_w_MEM;
+
 reg         res_from_mem_MEM;
 reg         gr_we_MEM;
 reg  [ 4:0] dest_MEM;
@@ -444,7 +611,9 @@ always @(posedge clk) begin
     alu_result_MEM   <= alu_result;
     res_from_mem_MEM <= res_from_mem_EX;
     gr_we_MEM        <= gr_we_EX;
-    dest_MEM         <= dest_EX;    
+    dest_MEM         <= dest_EX;  
+    inst_ld_w_MEM    <= inst_ld_w_EX;
+    inst_st_w_MEM    <= inst_st_w_EX;  
 end
 
 
@@ -467,6 +636,9 @@ reg  [31:0] pc_WB;
 reg  [31:0] alu_result_WB;
 reg  [31:0] mem_result_WB;
 
+reg         inst_ld_w_WB;
+reg         inst_st_w_WB;
+
 reg         res_from_mem_WB;
 reg         gr_we_WB;
 reg  [ 4:0] dest_WB;
@@ -487,6 +659,8 @@ always @(posedge clk) begin
         res_from_mem_WB <= res_from_mem_MEM;
         gr_we_WB        <= gr_we_MEM;
         dest_WB         <= dest_MEM;
+        inst_ld_w_WB    <= inst_ld_w_MEM;
+        inst_st_w_WB    <= inst_st_w_MEM;
     end
 end
 
